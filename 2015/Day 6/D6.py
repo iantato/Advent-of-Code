e27c459a81046e66849c01b10fafc13870bdd022
iantato <margiehouses@gmail.com>
2023-09-14 14:18:38 +0800

    Advent of Code 2015

--- 2015/Day 6/D6.py
+++ 2015/Day 6/D6.py
@@ -2,169 +2,94 @@
 from os import getcwd
 
 # Day 6 --- Part One --- Probably a Fire Hazard
-    # Create grid.
-def grid(size : int) -> list:
-    
-    # Grid needs to be a dictionary so that we can determine
-    # whether a certain position's light is turned on or off.
-    grid = dict({})
-    
-    # Create a grid with x, y positions
-    for y in range(size):
-        for x in range(size):
-            grid[(x, y)] = 0
-        
-    return grid
-    
     # Solve the lights problem.
-def lights(instructions : list, grid : dict) -> int:
+def lights(instructions : list) -> int:
     
-    # Create a copy of the grid dictionary.
-    lights = grid.copy()
+    # Generate a 1000x1000 grid by using multi-dimensional arrays.
+    grid = [[0]*1000 for _ in range(1000)]
+    # Initialize the opened lights counter.
+    lights = 0
     
     for line in instructions:
         
-        line = line[:-1].split(' ')
-        # 'toggle' or 'turn'.
-        action = line[0]
-        
-        # Check if the keyword is 'toggle' or 'turn'.
-        match action:
-            
-            case 'toggle':
-                
-                # Create a tuple with integers inside of them for the
-                # coordinates.
-                ini_coord = tuple(map(int, line[1].split(',')))
-                max_coord = tuple(map(int, line[3].split(',')))
-                
-                ini_x = ini_coord[0]
-                ini_y = ini_coord[1]
-                max_x = max_coord[0]
-                max_y = max_coord[1]
-                
-                # Loop the grid cells to toggle them on or off.
-                for x in range(ini_x, max_x + 1):
-                    for y in range(ini_y, max_y + 1):
-                        
-                        # Check whether the light in a cell is already turned on or off
-                        # and then toggle them off or on depending on their value.
-                        # 1 = on | 2 = off.
-                        mode = lights.get((x, y))
+        # Split the line into four different phrases from the right.
+        # 'turn/toggle on/off', 'initial coords', through, 'maximum coords'.
+        # The 3 is used to indicate the 1st and 2nd index to be combined and stop 
+        # at the 3rd.
+        action, ini_coords, _, max_coords = line.strip().rsplit(' ', 3)
+        # Turn the string coordinates into a tupled integer.
+        x_ini, y_ini = tuple(map(int, ini_coords.rsplit(',')))
+        x_max, y_max = tuple(map(int, max_coords.rsplit(',')))
 
-                        if mode == 0:
-                            lights[(x, y)] = 1
+        for x in range(x_ini, x_max + 1):
+            for y in range(y_ini, y_max + 1):
+                
+                match action:
+                    
+                    # 1 = On.
+                    case 'turn on':
+                        grid[x][y] = 1
+                    
+                    # 0 = Off.
+                    case 'turn off':
+                        grid[x][y] = 0
+                    
+                    case 'toggle':
+                        
+                        if grid[x][y] == 0:
+                            grid[x][y] = 1
                         else:
-                            lights[(x, y)] = 0
-                
-            case 'turn':
-                
-                # 'on' or 'off'
-                mode = line[1]
-                
-                # Create a tuple with integers inside of them for the
-                # coordinates.
-                ini_coord = tuple(map(int, line[2].split(',')))
-                max_coord = tuple(map(int, line[4].split(',')))
-                
-                ini_x = ini_coord[0]
-                ini_y = ini_coord[1]
-                max_x = max_coord[0]
-                max_y = max_coord[1]
-                
-                # Check if you have to turn the lights 'off' or 'on'
-                match mode:
-                    
-                    case 'on':
-                        
-                        # Sets the light of the cell into 'on' state.
-                        # 1 = on.
-                        for x in range(ini_x, max_x + 1):
-                            for y in range(ini_y, max_y + 1):
-                                lights[(x, y)] = 1
-                    
-                    case 'off':
-                        
-                        # Sets the light of the cell into 'off' state.
-                        # 0 = off.
-                        for x in range(ini_x, max_x + 1):
-                            for y in range(ini_y, max_y + 1):
-                                lights[(x, y)] = 0
+                            grid[x][y] = 0
     
-    return sum(lights.values())
+    for rows in grid:
+        lights += sum(rows)
+        
+    return lights
 
 # Day 6 --- Part Two --- Probably a Fire Hazard
-def brightness(instructions : list, grid : dict) -> int:
+def brightness(instructions: list) -> int:
     
-    # Create a copy of the grid dictionary.
-    lights = grid.copy()
+    grid = [[0]*1000 for _ in range(1000)]
+    brightness = 0
     
     for line in instructions:
         
-        line = line[:-1].split(' ')
-        # 'toggle' or 'turn'.
-        action = line[0]
+        # Split the line into four different phrases from the right.
+        # 'turn/toggle on/off', 'initial coords', through, 'maximum coords'.
+        # The 3 is used to indicate the 1st and 2nd index to be combined and stop 
+        # at the 3rd.
+        action, ini_coords, _, max_coords = line.strip().rsplit(' ', 3)
+        # Turn the string coordinates into a tupled integer.
+        x_ini, y_ini = tuple(map(int, ini_coords.rsplit(',')))
+        x_max, y_max = tuple(map(int, max_coords.rsplit(',')))
         
-        # Check if the keyword is 'toggle' or 'turn'.
-        match action:
-            
-            case 'toggle':
+        for x in range(x_ini, x_max + 1):
+            for y in range(y_ini, y_max + 1):
                 
-                # Create a tuple with integers inside of them for the
-                # coordinates.
-                ini_coord = tuple(map(int, line[1].split(',')))
-                max_coord = tuple(map(int, line[3].split(',')))
-                
-                ini_x = ini_coord[0]
-                ini_y = ini_coord[1]
-                max_x = max_coord[0]
-                max_y = max_coord[1]
-                
-                
-                for x in range(ini_x, max_x + 1):
-                    for y in range(ini_y, max_y + 1):
-                        lights[(x, y)] += 2
-                
-            case 'turn':
-                
-                # 'on' or 'off'
-                mode = line[1]
-                
-                # Create a tuple with integers inside of them for the
-                # coordinates.
-                ini_coord = tuple(map(int, line[2].split(',')))
-                max_coord = tuple(map(int, line[4].split(',')))
-                
-                ini_x = ini_coord[0]
-                ini_y = ini_coord[1]
-                max_x = max_coord[0]
-                max_y = max_coord[1]
-                
-                # Check if you have to turn the lights 'off' or 'on'
-                match mode:
+                match action:
                     
-                    case 'on':
-                        
-                        for x in range(ini_x, max_x + 1):
-                            for y in range(ini_y, max_y + 1):
-                                lights[(x, y)] += 1
+                    # On = +1.
+                    case 'turn on':
+                        grid[x][y] += 1
                     
-                    case 'off':
+                    # Off = -1 | Minimum of zero.
+                    case 'turn off':
+                        if grid[x][y] > 0:
+                            grid[x][y] -= 1
+                    
+                    case 'toggle':
                         
-                        for x in range(ini_x, max_x + 1):
-                            for y in range(ini_y, max_y + 1):
-                                
-                                if lights[(x, y)] > 0:
-                                    lights[(x, y)] -= 1
-                
-    return sum(lights.values())
+                        grid[x][y] += 2
+    
+    for rows in grid:
+        brightness += sum(rows)
+        
+    return brightness
 
 
 
 DIR = getcwd() + '\\2015\\input'
 file = open(DIR + '\\day6.txt').readlines()
 
-grid = grid(1000)
-
-print(lights(file, grid))
-print(brightness(file, grid))
+print(lights(file))
+print(brightness(file))
